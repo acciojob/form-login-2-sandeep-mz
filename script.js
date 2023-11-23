7c7e77531477edcb304d706e0ba5f8cfacbc5035
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-11-23 17:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent the default form submission behavior
+
+      // Get form data
+      const formData = new FormData(this);
+      let result = "";
+
+      // Construct the result string
+      for (let [key, value] of formData.entries()) {
+        result += key + ": " + value + "\n";
+      }
+
+      // Display the result using alert with the specified format
+      alert(result);
+    });
